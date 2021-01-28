cb424e111c0200596f28a0e910098aa14e68b023
Loic <loic.brassart@gmail.com>
2021-01-28 18:11:34 +0100

    Set up style resetter in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Reset } from 'styled-reset'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
