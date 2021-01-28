16346bf6369ef3016924e75afa9c7969ac14d3e1
Loic <loic.brassart@gmail.com>
2021-01-28 18:10:43 +0100

    Add styled-reset ; Set up style file for Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
+import { Header } from "./components";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import { SHeader } from "./style";
+
+const Header = () => {
+    return <SHeader>
+        <img src="" alt="" />
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>???</li>
+            </ul>
+        </nav>
+    </SHeader>;
+};
+export default Header;
